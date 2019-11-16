8de76de78f5a59189dcfd0b6f95bc1d34ee50b54
yusuke.matsuo <beautifulsaturday1215@gmail.com>
2019-11-16 02:02:54 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to root_url
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      flash[:success] = 'プロフィールを更新しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'プロフィールを更新できませんでした。'
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile, :image)
+  end
+end
