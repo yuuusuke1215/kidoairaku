b17930b72fba653c79efc9787fb1e272539e1346
yusuke.matsuo <beautifulsaturday1215@gmail.com>
2019-12-10 05:43:50 +0000

    for heroku2

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Kidoiraku
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -30,5 +31,6 @@ module Kidoiraku
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.time_zone = 'Tokyo'
+    config.assets.initialize_on_precompile = false
   end
 end
